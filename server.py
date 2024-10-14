4dfdc72fb4f796e5feedaae9d4235d4cc5c4168e
NedWithNoHead <tinle@LGGram16>
2024-10-14 14:19:42 -0700

    added a login front page

---
+++ server.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = MyHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving Horn Pub at http://localhost:{PORT}")
+    httpd.serve_forever()
